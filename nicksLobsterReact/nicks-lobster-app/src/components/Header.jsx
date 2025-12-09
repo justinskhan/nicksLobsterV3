a9084602375825890fe8e8a30e8e561f3ab6501c
justinskhan <114786398+justinskhan@users.noreply.github.com>
2025-12-08 23:06:45 -0500

    cart fixes

--- nicksLobsterReact/nicks-lobster-app/src/components/Header.jsx
+++ nicksLobsterReact/nicks-lobster-app/src/components/Header.jsx
@@ -1,19 +1,35 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
-export default function Header() 
-{
+export default function Header() {
+  const { cart } = useContext(CartContext);
+
+  // total number of items in cart calculation
+  const totalItems = cart.reduce(
+    (sum, item) => sum + (item.quantity || 1),
+    0
+  );
+
   return (
     <nav>
-      <div className="myLogo"> {/* picture in header*/}
+      <div className="myLogo">
         <img src="/images/icon.png" alt="Nick's Lobster Logo" />
       </div>
 
-      <ul> {/* list of links for header*/}
+      <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Menu</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/cart">Cart</Link></li> 
+
+        {/* Cart link with badge */}
+        <li className="cart-link-wrapper">
+          <Link to="/cart">Cart</Link>
+          {totalItems > 0 && (
+            <span className="cart-badge">{totalItems}</span>
+          )}
+        </li>
       </ul>
     </nav>
   );
